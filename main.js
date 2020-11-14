202a66426ca12edb65a0a13c8dfb0893e8835830
tiger-fish <25111698+tiger-fish@users.noreply.github.com>
2020-11-14 14:45:30 -0500

    added h1, p element
    p = movie.description
    h1 = movie.link

--- main.js
+++ main.js
@@ -1,29 +1,66 @@
-let request = new XMLHttpRequest()
+const app = document.getElementById('root')
 
+const logo = document.createElement('img')
+logo.src = 'logo.png'
+
+const container = document.createElement('div')
+container.setAttribute('class', 'container')
+
+app.appendChild(logo)
+app.appendChild(container)
+
+
+// Create a request variable and assign a new XMLHttpRequest object to it.
+var request = new XMLHttpRequest()
+
+// Open a new connection, using the GET request on the URL endpoint
 request.open('GET', 'https://ghibliapi.herokuapp.com/films', true);
 
 request.onload = function () {
   // Begin accessing JSON data here
   let data = JSON.parse(this.response)
-
+ 
+  //404 is the most well-known response, meaning Not Found, and 200 OK is a successful request.
   if (request.status >= 200 && request.status < 400) {
     data.forEach((movie) => {
-      console.log(movie.title)
+      // Create a div with a card class
+      const card = document.createElement('div')
+      card.setAttribute('class', 'card')
+
+      const h1 = document.createElement('h1')
+      h1.textContent = movie.title
+      
+        // Create a p and set the text content to the film's description
+      const p = document.createElement('p')
+      
+      // Create link and set the text content to the json url
+      const link = document.createElement('a');
+      const linkText = document.createTextNode("read more");
+      link.title = "read more";
+      link.href = movie.url;
+      link.target = "_blank";
+      link.appendChild(linkText);
+      
+      
+      movie.description = movie.description.substring(0,300); // Limit to 300 chars
+      p.textContent = `${movie.description}... `
+      
+      // Append the cards to the container element
+      container.appendChild(card)
+      
+      // Each card will contain an h1 and a p
+      card.appendChild(h1)
+      card.appendChild(p)
+      
+      // Each p will have a link
+      p.appendChild(link)
     })
   } else {
-    console.log('error')
+    const errorMessage = document.createElement('marquee')
+    errorMessage.textContent = `Gah, it's not working!`
+    app.appendChild(errorMessage)
   }
 }
 
+// Send request
 request.send()
-
-const app = document.querySelector("#root");
-const logo = document.createElement('img');
-
-logo.src = "logo.png";
-
-const container = document.createElement('div');
-container.setAttribute('class', 'container');
-
-
-app.appendChild(logo);
